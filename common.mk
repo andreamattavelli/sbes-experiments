bfef389feb4e95b53699e895f47fc2c11ad4e787
mattavelli <mattavelli@62d94d92-d06f-44e7-bb87-548ab0999166>
2014-02-26 09:23:35 +0000

    added skeleton
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@7562 62d94d92-d06f-44e7-bb87-548ab0999166

---
+++ common.mk
@@ -0,0 +1,55 @@
+# -*- Makefile -*-
+##
+## Framework-wide configuration
+##
+## NOTE:  this file will be included by the specific makefiles
+##
+## the only thing that we assume here is that the specific makefile
+## defines the ROOT directory of the experimentation framework, which
+## is the directory where this file resides.
+
+##
+## ROOT PATHS
+##
+EXPERIMENTS=$(ROOT)/experiments
+LIBS=$(ROOT)/libs
+TOOLS=$(ROOT)/tools
+
+##
+## CONFIGURATIONS
+##
+default: all
+
+##
+## OTHER COMMANDS
+##
+UNAME=$(shell uname)
+SHELL:=/bin/bash
+
+##
+## JAVA
+##
+# Linux (research / allegro)
+JAVAROOT=/tools/jdk1.7.0_45
+# Mac (with java 1.6)
+#JAVAROOT=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents
+#JAVAROOT=/System/Library/Frameworks/JavaVM.framework/Versions/Current
+
+# set java paths depending on the operating system
+ifeq ($(UNAME),Darwin)
+JAVABINDIR=$(JAVAROOT)/Commands
+JAVALIBDIR=$(JAVAROOT)/Resources
+export JAVA_HOME=$(/usr/libexec/java_home)
+else
+JAVABINDIR=$(JAVAROOT)/bin
+JAVALIBDIR=$(JAVAROOT)/jre/lib
+export JAVA_HOME=$(JAVAROOT)
+endif
+
+JAVA=$(JAVABINDIR)/java
+JAVAC=$(JAVABINDIR)/javac
+JAR=$(JAVABINDIR)/jar
+
+## ANT
+ANT=/usr/bin/ant
+

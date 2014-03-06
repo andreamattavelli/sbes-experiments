2dd29ad0f1c1bda23126cbc963807901ac46b021
goffi <goffi@62d94d92-d06f-44e7-bb87-548ab0999166>
2014-03-06 09:28:06 +0000

    Updated initial scenarios for Java Stack.
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@7688 62d94d92-d06f-44e7-bb87-548ab0999166

--- experiments/java/stack/stack.util.Stack.peek()/initial_scenario/InitialScenario.java
+++ experiments/java/stack/stack.util.Stack.peek()/initial_scenario/InitialScenario.java
@@ -20,8 +20,12 @@ public class StackEvoSuiteTest {
   @Test
   public void test0()  throws Throwable  {
       Stack<Integer> stack0 = new Stack<Integer>();
-      Integer integer0 = new Integer(0);
+      Integer integer0 = new Integer(234);
+      Integer integer1 = new Integer(55);
+      Integer integer2 = new Integer(2);
       boolean boolean0 = stack0.add(integer0);
-      Integer integer1 = stack0.peek();
+      boolean boolean1 = stack0.add(integer1);
+      boolean boolean2 = stack0.add(integer2);
+      Integer integer3 = stack0.peek();
   }
 }

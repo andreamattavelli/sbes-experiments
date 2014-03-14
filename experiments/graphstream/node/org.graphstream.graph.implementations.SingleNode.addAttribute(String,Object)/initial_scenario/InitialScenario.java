9670997ed8bc7a29de084ab2aa6c27b34c26e5c8
mattavelli <mattavelli@62d94d92-d06f-44e7-bb87-548ab0999166>
2014-03-14 12:37:34 +0000

    fixed problems
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@7838 62d94d92-d06f-44e7-bb87-548ab0999166

--- experiments/graphstream/node/org.graphstream.graph.implementations.SingleNode.addAttribute(String,Object)/initial_scenario/InitialScenario.java
+++ experiments/graphstream/node/org.graphstream.graph.implementations.SingleNode.addAttribute(String,Object)/initial_scenario/InitialScenario.java
@@ -3,17 +3,16 @@ package org.graphstream.graph.implementations;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.graphstream.graph.implementations.AbstractNode;
-import org.graphstream.graph.implementations.MultiGraph;
-import org.graphstream.graph.implementations.MultiNode;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.graph.implementations.SingleNode;
 
 public class AbstractEdgeEvoSuiteTest {
 
   @Test
   public void test0()  throws Throwable  {
-      MultiGraph multiGraph0 = new MultiGraph("graph", true, true);
-      MultiNode multiNode0 = new MultiNode(multiGraph0, "node");
+      SingleGraph multiGraph0 = new SingleGraph("graph", true, true);
+      SingleNode multiNode0 = new SingleNode(multiGraph0, "node");
       Object[] ob = new Object[]{325, "a possible value", 42000};
       multiNode0.addAttribute("value", ob);
-       Object[] attrs = abstractNode0.getAttribute("value");
   }
 }

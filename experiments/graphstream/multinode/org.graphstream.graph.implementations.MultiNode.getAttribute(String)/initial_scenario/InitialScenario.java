0356b47c0ef26644b8b045fbf7834e9ca4a11bd3
mattavelli <mattavelli@62d94d92-d06f-44e7-bb87-548ab0999166>
2014-03-14 16:44:11 +0000

    fixed scenario
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@7848 62d94d92-d06f-44e7-bb87-548ab0999166

--- experiments/graphstream/multinode/org.graphstream.graph.implementations.MultiNode.getAttribute(String)/initial_scenario/InitialScenario.java
+++ experiments/graphstream/multinode/org.graphstream.graph.implementations.MultiNode.getAttribute(String)/initial_scenario/InitialScenario.java
@@ -11,7 +11,7 @@ public class MultiNodeEvoSuiteTest {
   @Test
   public void test0()  throws Throwable  {
       MultiGraph multiGraph0 = new MultiGraph("graph", true, true);
-	  MultiNode node = multiGraph0.addNode("aNode");
+	  MultiNode node = new MultiNode(multiGraph0, "aNode");
 	  Object[] ob = new Object[]{325, "a possible value", 42000};
 	  node.addAttribute("value", ob);
 	  Object[] attrs = node.getAttribute("value");

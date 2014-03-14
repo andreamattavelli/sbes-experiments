2d3c3e21b1869a380b73894dd9caa5dc166fd83e
mattavelli <mattavelli@62d94d92-d06f-44e7-bb87-548ab0999166>
2014-03-14 10:55:09 +0000

    fixed problems
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@7836 62d94d92-d06f-44e7-bb87-548ab0999166

--- experiments/graphstream/node/org.graphstream.graph.implementations.MultiNode.changeAttribute(String,Object)/initial_scenario/InitialScenario.java
+++ experiments/graphstream/node/org.graphstream.graph.implementations.MultiNode.changeAttribute(String,Object)/initial_scenario/InitialScenario.java
@@ -11,8 +11,8 @@ public class AbstractEdgeEvoSuiteTest {
   @Test
   public void test0()  throws Throwable  {
       MultiGraph multiGraph0 = new MultiGraph("graph", true, true);
-      AbstractNode abstractNode0 = multiGraph0.addNode("node");
+      MultiNode multiNode0 = new MultiNode(multiGraph0, "node");
       Object[] ob = new Object[]{325, "a possible value", 42000};
-      abstractNode0.setAttribute("value", ob);
+      multiNode0.changeAttribute("value", ob);
   }
 }
--- experiments/graphstream/node/org.graphstream.graph.implementations.MultiNode.getAttribute(String)/initial_scenario/InitialScenario.java
+++ experiments/graphstream/node/org.graphstream.graph.implementations.MultiNode.getAttribute(String)/initial_scenario/InitialScenario.java
@@ -11,9 +11,9 @@ public class AbstractEdgeEvoSuiteTest {
   @Test
   public void test0()  throws Throwable  {
       MultiGraph multiGraph0 = new MultiGraph("graph", true, true);
-      AbstractNode abstractNode0 = multiGraph0.addNode("node");
+      MultiNode multiNode0 = new MultiNode(multiGraph0, "node");
       Object[] ob = new Object[]{325, "a possible value", 42000};
-      abstractNode0.addAttribute("value", ob);
-      abstractNode0.getAttribute("value");
+      multiNode0.addAttribute("value", ob);
+       Object[] attrs = abstractNode0.getAttribute("value");
   }
 }

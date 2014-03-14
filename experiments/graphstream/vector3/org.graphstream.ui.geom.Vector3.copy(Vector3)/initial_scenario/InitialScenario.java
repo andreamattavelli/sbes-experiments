bc3a201e06fc9a5376fc13e5485f83e29586ea0b
goffi <goffi@62d94d92-d06f-44e7-bb87-548ab0999166>
2014-03-14 10:34:57 +0000

    Fixed initial scenarios for Vector3.
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@7835 62d94d92-d06f-44e7-bb87-548ab0999166

--- experiments/graphstream/vector3/org.graphstream.ui.geom.Vector3.copy(Vector3)/initial_scenario/InitialScenario.java
+++ experiments/graphstream/vector3/org.graphstream.ui.geom.Vector3.copy(Vector3)/initial_scenario/InitialScenario.java
@@ -9,7 +9,7 @@ public class Vector3EvoSuiteTest {
   @Test
   public void test0() throws Throwable  {
       Vector3 v1 = new Vector3();
-      v1.set(345.0d, 432.0d. 231.0d);
+      v1.set(345.0d, 432.0d, 231.0d);
       Vector3 v2 = new Vector3();
       v2.set(35.0d, 42.0d, 12.0d);
       v1.copy(v2);
--- experiments/graphstream/vector3/org.graphstream.ui.geom.Vector3.z()/initial_scenario/InitialScenario.java
+++ experiments/graphstream/vector3/org.graphstream.ui.geom.Vector3.z()/initial_scenario/InitialScenario.java
@@ -10,6 +10,6 @@ public class Vector3EvoSuiteTest {
   public void test0() throws Throwable  {
       Vector3 v1 = new Vector3();
       v1.set(345.0d, 432.0d, 123.0d);
-      double val = v1.y();
+      double val = v1.z();
   }
 }

570d62245206d7325fb6e57c5929bc671a7b153e
mattavelli <mattavelli@62d94d92-d06f-44e7-bb87-548ab0999166>
2014-03-14 16:22:55 +0000

    fixed scenario
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@7846 62d94d92-d06f-44e7-bb87-548ab0999166

--- experiments/java/stack/stack.util.Stack.pop()/initial_scenario/InitialScenario.java
+++ experiments/java/stack/stack.util.Stack.pop()/initial_scenario/InitialScenario.java
@@ -15,7 +15,7 @@ public class StackEvoSuiteTest {
       Stack<Integer> stack0 = new Stack<Integer>();
       Integer integer0 = new Integer(234);
       Integer integer1 = new Integer(55);
-      Integer integer2 = new Integer(2);
+      Integer integer2 = new Integer(-2);
       boolean boolean0 = stack0.add(integer0);
       boolean boolean1 = stack0.add(integer1);
       boolean boolean2 = stack0.add(integer2);

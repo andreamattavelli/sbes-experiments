dd32257599b1421e1807f00b57c5fe66c2b4d78e
goffi <goffi@62d94d92-d06f-44e7-bb87-548ab0999166>
2014-03-14 09:39:43 +0000

    Added initial scenarios for Vector3.
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@7834 62d94d92-d06f-44e7-bb87-548ab0999166

---
+++ experiments/graphstream/vector3/org.graphstream.ui.geom.Vector3.fill(double)/initial_scenario/InitialScenario.java
@@ -0,0 +1,14 @@
+package org.graphstream.ui.geom;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.graphstream.ui.geom.Vector3;
+
+public class Vector3EvoSuiteTest {
+
+  @Test
+  public void test0() throws Throwable  {
+      Vector3 v1 = new Vector3();
+      v1.fill(345.0);
+  }
+}

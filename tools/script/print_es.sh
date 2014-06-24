0dcbb14d3ef7fa4fafee02b63a3360e62457b62a
mattavelli <mattavelli@62d94d92-d06f-44e7-bb87-548ab0999166>
2014-06-24 15:22:51 +0000

    added script for data generation
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@8049 62d94d92-d06f-44e7-bb87-548ab0999166

---
+++ tools/script/print_es.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+graphstream=`find graphstream -maxdepth 4 -mindepth 4 -type f \( \( ! -ipath '*svn*' \) -a \( -iname "log" \) \)`
+java=`find java -maxdepth 4 -mindepth 4 -type f \( \( ! -ipath '*svn*' \) -a \( -iname "log" \) \)`
+
+rm -rf graphstream_es.txt
+rm -rf java_es.txt
+
+echo "Processing GraphStream.."
+for gs in $graphstream; do
+	echo "========== $gs" >> graphstream_es.txt
+
+	# get length of the log file
+	length=`wc -l < $gs`
+	# get initial position of equivalent sequences print
+	position=`grep -n "Equivalent sequences synthesized" $gs | cut -d ':' -f1`
+
+	if [ -z "$position" ]; then
+		echo "No equivalent sequence found" >> graphstream_es.txt
+	else
+		tail_index=`expr $length - $position`
+		head_index=`expr $tail_index - 2`
+
+		tail -n$tail_index $gs | head -n$head_index >> graphstream_es.txt
+
+		echo "" >> graphstream_es.txt
+	fi
+done
+
+echo "Processing java.Stack.."
+for jv in $java; do
+	echo "========== $jv" >> java_es.txt
+
+	# get length of the log file
+	length=`wc -l < $jv`
+	# get initial position of equivalent sequences print
+	position=`grep -n "Equivalent sequences synthesized" $jv | cut -d ':' -f1`
+
+	if [ -z "$position" ]; then
+		echo "No equivalent sequence found"  >> java_es.txt
+	else 
+		tail_index=`expr $length - $position`
+		head_index=`expr $tail_index - 2`
+
+		tail -n$tail_index $jv | head -n$head_index  >> java_es.txt
+
+		echo ""  >> java_es.txt
+	fi
+done
+
+echo "Done!"

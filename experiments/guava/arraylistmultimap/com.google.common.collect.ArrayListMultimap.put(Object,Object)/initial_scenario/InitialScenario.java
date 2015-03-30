c85dab2ab47f2e40d73942dc10a2de90c588556e
mattavelli <mattavelli@62d94d92-d06f-44e7-bb87-548ab0999166>
2015-03-30 12:45:15 +0000

    fixed scenario
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@9049 62d94d92-d06f-44e7-bb87-548ab0999166

--- experiments/guava/arraylistmultimap/com.google.common.collect.ArrayListMultimap.put(Object,Object)/initial_scenario/InitialScenario.java
+++ experiments/guava/arraylistmultimap/com.google.common.collect.ArrayListMultimap.put(Object,Object)/initial_scenario/InitialScenario.java
@@ -5,7 +5,13 @@
 package com.google.common.collect;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Test;
+
 import com.google.common.collect.ArrayListMultimap;
 
 public class StackEvoSuiteTest {

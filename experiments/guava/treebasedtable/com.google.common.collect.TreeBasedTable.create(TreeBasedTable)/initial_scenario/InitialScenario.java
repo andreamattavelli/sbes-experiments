760d82195453b5af0300d52c2514af54f7b1c6fd
mattavelli <mattavelli@62d94d92-d06f-44e7-bb87-548ab0999166>
2015-04-09 12:01:22 +0000

    added initial scenarios
    git-svn-id: http://star.inf.usi.ch/repos/code/SBES-Experiments/trunk@9117 62d94d92-d06f-44e7-bb87-548ab0999166

--- experiments/guava/treebasedtable/com.google.common.collect.TreeBasedTable.create(TreeBasedTable)/initial_scenario/InitialScenario.java
+++ experiments/guava/treebasedtable/com.google.common.collect.TreeBasedTable.create(TreeBasedTable)/initial_scenario/InitialScenario.java
@@ -6,9 +6,8 @@ package com.google.common.collect;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Collection;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -18,8 +17,9 @@ public class StackEvoSuiteTest {
 
   @Test
   public void test0()  throws Throwable  {
-    	HashMultiset<Integer> hashMultiset0 = HashMultiset.create();
-		Integer integer0 = new Integer(-18247);
-		int int0 = hashMultiset0.add(integer0, 3);
+    	Table<Integer, String, Character> table = TreeBasedTable.create();
+		table.put(0, "0", 'a');
+		table.put(1, "0", 'b');
+		TreeBasedTable<Integer, String, Character> hashBasedTable0 = TreeBasedTable.create((TreeBasedTable<Integer, String, ? extends Character>) table);
   }
 }
